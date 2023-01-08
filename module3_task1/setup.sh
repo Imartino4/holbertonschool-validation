533ea17d7d9588f4cfcfc30afb3c18b593b771ec
Imartino4 <ignacio.martacos@gmail.com>
2023-01-08 15:47:04 -0300

    Task 1 - new version for hugo

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
-dpkg -i hugo_0.79.0_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
 make build
