cfeed120a112df44f79b1ea03606482084c1fc0f
Imartino4 <ignacio.martacos@gmail.com>
2023-01-08 17:11:24 -0300

    lint update

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -3,5 +3,4 @@ sudo apt-get update
 sudo apt-get install -y make wget
 sudo wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-sudo apt install snapd
-sudo snap install mdl
+npm install -g markdownlint-cli
